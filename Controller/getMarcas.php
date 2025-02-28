0a244d7fe32497008bed10517c50ef50b3e7dfc8
root <zxxjakter@gmail.com>
2025-02-28 12:47:20 -0400

    listo los modulos de catalogos, terminados

--- Controller/getMarcas.php
+++ Controller/getMarcas.php
@@ -3,7 +3,7 @@ include '../Model/conexion.php';
 
 $conn = conectarDB("bdd_sisasc");
 
-$query = $conn->prepare("SELECT id_marca, marca_marca FROM marca WHERE status_marca = 1");
+$query = $conn->prepare("SELECT id_marca, marca_marca FROM marca WHERE status_marca = '1'");
 $query->execute();
 $result = $query->get_result();
 
